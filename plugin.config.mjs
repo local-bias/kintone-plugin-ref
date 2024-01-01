92bc9aa13b01dd2f784e6adf261ad981f9d961b6
Ribbit <echo07200503@gmail.com>
2024-01-01 14:15:13 +0900

    ⚙ Update plugin.config.js and add plugin.config.mjs

---
+++ plugin.config.mjs
@@ -0,0 +1,61 @@
+//@ts-check
+const hp = 'https://konomi.app/';
+const commonCdn = 'https://kintone-plugin.konomi.app/common';
+const localhost = 'https://127.0.0.1:9999';
+
+/** @type { import('@konomi-app/kintone-utilities').PluginConfig } */
+export default {
+  id: 'ribbit-kintone-plugin-ref',
+  pluginReleasePageUrl: `https://ribbit.konomi.app/kintone-plugin/`,
+  manifest: {
+    base: {
+      manifest_version: 1,
+      version: '1.1.0',
+      type: 'APP',
+      name: {
+        en: 'Related field display plugin',
+        ja: '関連フィールド表示プラグイン',
+        zh: '相关字段显示插件',
+      },
+      description: {
+        en: 'Display field information on the detail screen from an application that has a one-to-one relationship with specific key information',
+        ja: '特定のキー情報が一対一に紐づくようなアプリから、フィールド情報を詳細画面に表示します',
+        zh: '在与特定关键信息具有一对一关系的应用程序的详细信息屏幕上显示字段信息',
+      },
+      icon: 'icon.png',
+      homepage_url: { ja: hp, en: hp },
+      desktop: { js: [`${commonCdn}/desktop.js`], css: [] },
+      mobile: { js: [`${commonCdn}/desktop.js`], css: [] },
+      config: {
+        html: 'config.html',
+        js: [`${commonCdn}/config.js`],
+        css: [],
+        required_params: [],
+      },
+    },
+    dev: {
+      desktop: {
+        js: [`${localhost}/dist/dev/desktop/index.js`],
+        css: [`${localhost}/dist/dev/desktop.css`],
+      },
+      mobile: {
+        js: [`${localhost}/dist/dev/desktop/index.js`],
+        css: [`${localhost}/dist/dev/desktop.css`],
+      },
+      config: {
+        js: [`${localhost}/dist/dev/config/index.js`],
+        css: [`${localhost}/dist/dev/config.css`],
+      },
+    },
+    prod: {
+      desktop: { js: ['desktop.js'], css: ['desktop.css'] },
+      mobile: { js: ['desktop.js'], css: ['desktop.css'] },
+      config: { js: ['config.js'], css: ['config.css'] },
+    },
+    standalone: {
+      desktop: { js: ['desktop.js'], css: ['desktop.css'] },
+      mobile: { js: ['desktop.js'], css: ['desktop.css'] },
+      config: { js: ['config.js'], css: ['config.css'] },
+    },
+  },
+};
